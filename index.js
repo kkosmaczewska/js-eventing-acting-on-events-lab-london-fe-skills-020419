ba8872094f299daf348f6e559a6cffd77749f79c
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-04-03 19:11:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+var dodger = document.getElementById('dodger');
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
- 
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+  if (left >= 360) {
+    dodger.style.left = `${left + 0}px`;
   }
+  else if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
   
-}document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function(e) {
   if (e.which === 39) {
     moveDodgerRight();
   }
-})
+});
